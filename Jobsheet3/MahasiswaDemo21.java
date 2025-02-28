504bc5098e06dd38cdc9dd08940aaf746c01e93a
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2025-02-28 15:24:16 +0700

    Add files via upload

--- Jobsheet3/MahasiswaDemo21.java
+++ Jobsheet3/MahasiswaDemo21.java
@@ -23,10 +23,7 @@ public class MahasiswaDemo21 {
         }
             for (int i=0; i < 3; i++){
                 System.out.println("Data Mahasiswa ke-" + (i+1));
-                System.out.println("NIM      : "+ arrayOfMahasiswa[i].nim);
-                System.out.println("Nama     : "+ arrayOfMahasiswa[i].nama);
-                System.out.println("Kelas    : "+ arrayOfMahasiswa[i].kelas);
-                System.out.println("IPK      : "+ arrayOfMahasiswa[i].ipk);
+                arrayOfMahasiswa[i].cetakInfo();
                 System.out.println("--------------------------------");
             }   
     }
