eab49df760eec9518ed719c46853dc1b765d571e
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2025-03-01 11:14:30 +0700

    perubahan pertanyaan 2,3

--- Jobsheet3/MataKuliahDemo21.java
+++ Jobsheet3/MataKuliahDemo21.java
@@ -1,34 +1,32 @@
 import java.util.Scanner;
-public class MataKuliahDemo21{
-    public static void main(String[] args){
+public class MataKuliahDemo21 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MataKuliah21[] arrayOfMataKuliah21 = new MataKuliah21[3];
-        String kode, nama, dummy;
+        String kode, nama, dosenPengajar;
         int sks, jumlahJam;
 
-        for(int i=0; i < 3; i++){
-            System.out.println("Masukkan Data Matakuliah ke-" + (i+1));
-            System.out.print("Kode        : ");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode           : ");
             kode = sc.nextLine();
-            System.out.print("Nama        : ");
+            System.out.print("Nama           : ");
             nama = sc.nextLine();
-            System.out.print("SKS         : ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam  : ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
+            System.out.print("SKS            : ");
+            sks = Integer.parseInt(sc.nextLine());
+            System.out.print("Jumlah Jam     : ");
+            jumlahJam = Integer.parseInt(sc.nextLine());
+            System.out.print("Dosen Pengajar : ");
+            dosenPengajar = sc.nextLine();
             System.out.println("-----------------------------");
 
             arrayOfMataKuliah21[i] = new MataKuliah21(kode, nama, sks, jumlahJam);
-        }   
-        for (int i=0; i < 3; i++){
-            System.out.println("DATA MATAKULIAH KE-" + (i+1));
-            System.out.println("Kode        : "+ arrayOfMataKuliah21[i].kode);
-            System.out.println("Nama        : "+ arrayOfMataKuliah21[i].nama);
-            System.out.println("SKS         : "+ arrayOfMataKuliah21[i].sks);
-            System.out.println("Jumlah Jam  : "+ arrayOfMataKuliah21[i].jumlahJam);
-            System.out.println("-------------------");
+            arrayOfMataKuliah21[i].tambahData(dosenPengajar);
         }
-     }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("DATA MATAKULIAH KE-" + (i + 1));
+            arrayOfMataKuliah21[i].cetakInfo();
+        }
+    }
 }
