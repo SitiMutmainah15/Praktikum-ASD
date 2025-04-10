e8151e6d3766d7c5d2046e832dff26e9e1b4055c
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2025-04-10 13:47:59 +0700

    Add files via upload

---
+++ Jobsheet7/DosenDemo21.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class DosenDemo21 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DataDosen21 data = new DataDosen21();
+
+        while (true) {
+            System.out.println("\nMENU:");
+            System.out.println("1. Tambah Data Dosen");
+            System.out.println("2. Tampil Data Dosen");
+            System.out.println("3. Sorting ASC (Usia termuda ke tertua)");
+            System.out.println("4. Sorting DSC (Usia tertua ke termuda)");
+            System.out.println("5. PencarianDataSequential21 (Berdasarkan Nama)");
+            System.out.println("6. PencarianDataBinary21 (Berdasarkan Usia)");
+            System.out.println("7. Keluar");
+            System.out.print("Pilih menu: ");
+
+            int pilihan = input.nextInt();
+            input.nextLine();
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan Kode Dosen: ");
+                    String kode = input.nextLine();
+                    System.out.print("Masukkan Nama Dosen: ");
+                    String nama = input.nextLine();
+                    System.out.print("Jenis Kelamin (true = Laki-laki, false = Perempuan): ");
+                    boolean jk = input.nextBoolean();
+                    System.out.print("Masukkan Usia: ");
+                    int usia = input.nextInt();
+                    input.nextLine();
+                    data.tambah(new Dosen21(kode, nama, jk, usia));
+                    break;
+                case 2:
+                    data.tampil();
+                    break;
+                case 3:
+                    data.SortingASC();
+                    break;
+                case 4:
+                    data.sortingDSC();
+                    break;
+                case 5:
+                    System.out.print("Masukkan nama dosen yang dicari: ");
+                    String cariNama = input.nextLine();
+                    data.PencarianDataSequential21(cariNama); 
+                    break;
+                case 6:
+                    System.out.print("Masukkan usia dosen yang dicari: ");
+                    int cariUsia = input.nextInt();
+                    data.PencarianDataBinary21(cariUsia); 
+                    break;
+                case 7:
+                    System.out.println("Terima kasih! Program selesai.");
+                    return;
+                default:
+                    System.out.println("Pilihan tidak valid!");
+            }
+        }
+    }
+}
